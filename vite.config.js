b766094418b94c24db19951ba0d7b402271abb6f
st174308 <st174308@stud.uni-stuttgart.de>
2026-04-19 19:00:15 +0200

    Create github workflow to publish site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  // If we are building for production, use the repo name. 
+  // If we are just running 'npm run dev', use the root '/'.
+  base: command === 'build' ? '/orbitone/' : '/',
+}))
